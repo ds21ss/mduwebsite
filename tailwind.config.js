ae013d148802c6f2743e1bca31912815e0dcf608
ds21ss <bruce.hudson@ggc.scot.nhs.uk>
2022-09-09 16:40:58 +0100

    new nav bar, common header etc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
